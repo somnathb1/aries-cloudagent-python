4e17e9990c5c6b9ec576989f7867b5c012024729
Somnath Banerjee <somnathb@crubn.com>
2021-09-15 14:15:53 +0530

    replace with custom postgres

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-aca-py start --inbound-transport http 127.0.0.1 11020 --outbound-transport http --admin 127.0.0.1  11021  --wallet-storage-config "{\"url\":\"10.15.80.5:5432\",\"wallet_scheme\":\"DatabasePerWallet\"}" --wallet-storage-creds "{\"account\":\"postgres\",\"password\":\"Ssi@2021\",\"admin_account\":\"postgres\",\"admin_password\":\"Ssi@2021\"}"&
+aca-py start --inbound-transport http 127.0.0.1 11020 --outbound-transport http --admin 127.0.0.1  11021  --wallet-storage-config "{\"url\":\"34.132.23.213:5432\",\"wallet_scheme\":\"DatabasePerWallet\"}" --wallet-storage-creds "{\"account\":\"postgres\",\"password\":\"mysecretpassword\",\"admin_account\":\"postgres\",\"admin_password\":\"mysecretpassword\"}" &
 
 nginx -c "/etc/nginx/conf.d/default.conf" &
 
