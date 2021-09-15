5ed858335bf7c90e4853af46f12e7cd579c783fd
Somnath Banerjee <somnathb@crubn.com>
2021-09-15 14:24:39 +0530

    Remove wallet creds from start sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-aca-py start --inbound-transport http 127.0.0.1 11020 --outbound-transport http --admin 127.0.0.1  11021  --wallet-storage-config "{\"url\":\"34.132.23.213:5432\",\"wallet_scheme\":\"DatabasePerWallet\"}" --wallet-storage-creds "{\"account\":\"postgres\",\"password\":\"mysecretpassword\",\"admin_account\":\"postgres\",\"admin_password\":\"mysecretpassword\"}" &
+aca-py start --inbound-transport http 127.0.0.1 11020 --outbound-transport http --admin 127.0.0.1  11021 &
 
 nginx -c "/etc/nginx/conf.d/default.conf" &
 
