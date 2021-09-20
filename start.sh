9795f0caba79f5ebd3fe46e1525829eee59c3954
Somnath Banerjee <somnathb@crubn.com>
2021-09-21 00:40:52 +0530

    Add &

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo 1 | aca-py start --inbound-transport http 0.0.0.0 11020 --outbound-transport http --admin 0.0.0.0  11021 --auto-provision
+echo 1 | aca-py start --inbound-transport http 0.0.0.0 11020 --outbound-transport http --admin 0.0.0.0  11021 --auto-provision &
 
 nginx -c "/etc/nginx/conf.d/default.conf" &
 
